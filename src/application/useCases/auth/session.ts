21298229c521009e99d552873e2eb135bc25302f
Taylor Kelley <taylorgkelley@outlook.com>
2024-12-03 16:53:21 -0600

    refactoring

--- src/application/useCases/auth/session.ts
+++ src/application/useCases/auth/session.ts
@@ -3,102 +3,102 @@ import jwt from 'jsonwebtoken';
 import { User } from '../../../domain/entities/User';
 import db from '../../../infrastructure/database/drizzle';
 import { eq } from 'drizzle-orm';
-import usersTable from '../../../infrastructure/database/drizzle/schema/users.schema';
-import { AppError } from '../../../shared/errors/appError';
+import { usersTable } from '../../../infrastructure/database/drizzle/schema';
+import { AppError } from '../../../domain/entities/AppError';
 
 export type RefreshTokenData = {
-	userId: string;
-	refreshTokenVersion?: number;
+  userId: string;
+  refreshTokenVersion?: number;
 };
 
 export type AccessTokenData = {
-	userId: string;
+  userId: string;
 };
 
 const cookieSettings = {
-	httpOnly: true,
-	sameSite: 'lax',
+  httpOnly: true,
+  sameSite: 'lax',
 } as const;
 
 const createAuthTokens = (
-	user: User
+  user: User
 ): { refreshToken: string; accessToken: string } => {
-	const refreshToken = jwt.sign(
-		{ userId: user.id, refreshTokenVersion: user.refreshTokenVersion },
-		process.env.REFRESH_TOKEN_SECRET!,
-		{
-			expiresIn: '30d',
-		}
-	);
+  const refreshToken = jwt.sign(
+    { userId: user.id, refreshTokenVersion: user.refreshTokenVersion },
+    process.env.REFRESH_TOKEN_SECRET!,
+    {
+      expiresIn: '30d',
+    }
+  );
 
-	const accessToken = jwt.sign(
-		{ userId: user.id },
-		process.env.ACCESS_TOKEN_SECRET!,
-		{
-			expiresIn: '15min',
-		}
-	);
+  const accessToken = jwt.sign(
+    { userId: user.id },
+    process.env.ACCESS_TOKEN_SECRET!,
+    {
+      expiresIn: '15min',
+    }
+  );
 
-	return { refreshToken, accessToken };
+  return { refreshToken, accessToken };
 };
 
 export const createSession = (res: Response, user: User) => {
-	const { accessToken, refreshToken } = createAuthTokens(user);
-	res.cookie('id', accessToken, cookieSettings);
-	res.cookie('rid', refreshToken, cookieSettings);
-	return;
+  const { accessToken, refreshToken } = createAuthTokens(user);
+  res.cookie('id', accessToken, cookieSettings);
+  res.cookie('rid', refreshToken, cookieSettings);
+  return;
 };
 
 export const clearAuthCookies = (res: Response) => {
-	res.clearCookie('id', cookieSettings);
-	res.clearCookie('rid', cookieSettings);
+  res.clearCookie('id', cookieSettings);
+  res.clearCookie('rid', cookieSettings);
 };
 
 export const checkTokens = async (
-	accessToken: string,
-	refreshToken: string
+  accessToken: string,
+  refreshToken: string
 ) => {
-	try {
-		// verify
-		const data = <AccessTokenData>(
-			jwt.verify(accessToken, process.env.ACCESS_TOKEN_SECRET!)
-		);
+  try {
+    // verify
+    const data = <AccessTokenData>(
+      jwt.verify(accessToken, process.env.ACCESS_TOKEN_SECRET!)
+    );
 
-		// get userId from token data
-		return {
-			userId: data.userId,
-		};
-	} catch {
-		// token is expired or signed with a different secret
-		// so now check refresh token
-	}
+    // get userId from token data
+    return {
+      userId: data.userId,
+    };
+  } catch {
+    // token is expired or signed with a different secret
+    // so now check refresh token
+  }
 
-	if (!refreshToken) {
-		throw new AppError(401, 'Unauthorized');
-	}
+  if (!refreshToken) {
+    throw new AppError(401, 'Unauthorized');
+  }
 
-	// Verify refresh token
-	let data;
-	try {
-		data = <RefreshTokenData>(
-			jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET!)
-		);
-	} catch {
-		throw new AppError(401, 'Unauthorized');
-	}
+  // Verify refresh token
+  let data;
+  try {
+    data = <RefreshTokenData>(
+      jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET!)
+    );
+  } catch {
+    throw new AppError(401, 'Unauthorized');
+  }
 
-	// Get user
-	const user = await db.query.usersTable.findFirst({
-		where: eq(usersTable.id, data.userId),
-	});
+  // Get user
+  const user = (
+    await db.select().from(usersTable).where(eq(usersTable.id, data.userId))
+  ).at(0);
 
-	// Check refresh token version
-	if (!user || user.refreshTokenVersion !== data.refreshTokenVersion) {
-		throw new AppError(401, 'Unauthorized');
-	}
+  // Check refresh token version
+  if (!user || user.refreshTokenVersion !== data.refreshTokenVersion) {
+    throw new AppError(401, 'Unauthorized');
+  }
 
-	return {
-		userId: data.userId,
-		user,
-	};
+  return {
+    userId: data.userId,
+    user,
+  };
 };
--- src/infrastructure/database/drizzle/index.ts
+++ src/infrastructure/database/drizzle/index.ts
@@ -2,18 +2,21 @@ import 'dotenv/config';
 import { drizzle } from 'drizzle-orm/node-postgres';
 import { Pool } from 'pg';
 import { bakeriesTable } from './schema/bakeries.schema';
+import { usersTable } from './schema';
 
 const pool = new Pool({
-	connectionString: process.env.DATABASE_URL!,
+  connectionString: process.env.DATABASE_URL!,
 });
 const db = drizzle(pool);
 
 type DbSchema = {
-	bakeries: typeof bakeriesTable;
+  bakeries: typeof bakeriesTable;
+  users: typeof usersTable;
 };
 
 export const dbType: DbSchema = {
-	bakeries: bakeriesTable,
+  bakeries: bakeriesTable,
+  users: usersTable,
 };
 
 export default db;
